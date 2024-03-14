cb9ed908732baad4ff706079125f0552e60419f3
Prabhat Srivastava <pvtsvt@gmail.com>
2024-03-14 09:40:40 +0530

    some more changes to Binary Search

--- binarySearch.py
+++ binarySearch.py
@@ -14,6 +14,8 @@ def BinarySearch(seq,v):
                 seq = seq[midPos:]
                 midPos = len(seq)//2
                 print(f"midPos inside while loops is {midPos}")
+        if seq[midPos] == v:
+            return "Found in the list"
         print(midPos)
     return "Not Found and this one should go to Feature001"
         
