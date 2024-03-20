5e4c186e8d026156dd2860b9988000cc84f43f5c
Pvtsvt <pvtsvt@gmail.com>
2024-03-21 01:19:25 +0530

    reformatting

--- binarySearch.py
+++ binarySearch.py
@@ -1,25 +1,24 @@
-def BinarySearch(seq,v): 
+def BinarySearch(seq, v):
     seq = sorted(seq)
-    midPos = len(seq)//2  #integer division
+    midPos = len(seq) // 2  # integer division
     print(midPos)
-    while(midPos > 0):
-        if seq[midPos] == v: 
+    while midPos > 0:
+        if seq[midPos] == v:
             return "Found in the List"
-        else: 
-            if seq[midPos] < v: 
+        else:
+            if seq[midPos] < v:
                 seq = seq[:midPos]
-                midPos = len(seq)//2
+                midPos = len(seq) // 2
                 print(f"midPos inside while loops is {midPos}")
-            else: 
+            else:
                 seq = seq[midPos:]
-                midPos = len(seq)//2
+                midPos = len(seq) // 2
                 print(f"midPos inside while loops is {midPos}")
         if seq[midPos] == v:
             return "Found in the list"
         print(midPos)
     return "Not Found and this one should go to Feature001"
-        
 
 
-l = [34,56,243,4,6,0,5,366,67,84,9,254,556,7]
-print(BinarySearch(l,5))
+l = [34, 56, 243, 4, 6, 0, 5, 366, 67, 84, 9, 254, 556, 7]
+print(BinarySearch(l, 5))
